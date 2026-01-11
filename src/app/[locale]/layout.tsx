bfc6c9956a62af1e977ab1506dc6143b29127dc8
Mahmoud Medhat <M7moooud.17@gmail.com>
2026-01-11 23:16:51 +0200

    Working on scrolling.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,7 +6,6 @@ import { notFound } from 'next/navigation';
 import { getMessages } from 'next-intl/server';
 import Navbar from "@/app/common/Navbar";
 import Footer from "@/app/common/Footer";
-// import ScrollToTop from "@/utils/ScrollToTop";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,25 +29,26 @@ export default async function RootLayout({
   params
 }: Readonly<{
   children: React.ReactNode;
-  params: { locale: string }
+  params: Promise<{ locale: string }>
 }>) {
+
+  const { locale } = await params;
 
   let messages;
 
   try {
-    messages = await getMessages({ locale: params.locale });
+    messages = await getMessages({ locale: locale });
   } catch (error) {
     return notFound();
   }
 
 
   return (
-    <html lang={params.locale} dir={params.locale === "ar" ? "rtl" : "ltr"}>
+    <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NextIntlClientProvider messages={messages}>
-          {/* <ScrollToTop /> */}
           <Navbar />
           {children}
           <Footer />
